5b4331782a048a5b74b53b43feeca111ca8a5efb
Woody3097 <nicolay.danch@gmail.com>
2023-05-02 22:49:27 +0300

    main: updates

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MainComponent } from './main.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,14 @@ const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent,
+      },
+      {
+        path: 'sign-up',
+        component: SignUpComponent,
+      },
+      {
+        path: 'sign-in',
+        component: SignInComponent,
       },
       {
         path: '**',
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -4,16 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { MatIconModule } from '@angular/material/icon';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    MainRoutingModule
-  ]
+  declarations: [MainComponent, HomeComponent, SignUpComponent, SignInComponent],
+  imports: [CommonModule, MainRoutingModule, MatIconModule],
 })
-export class MainModule { }
+export class MainModule {}
