1ecea5f1d790268aa0d15124496e8bbf57b0dd88
Woody3097 <nicolay.danch@gmail.com>
2023-05-14 15:44:49 +0300

    main: updates

--- src/app/modules/main/pages/home/pizza-add/pizza-add.component.ts
+++ src/app/modules/main/pages/home/pizza-add/pizza-add.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-pizza-add',
   templateUrl: './pizza-add.component.html',
-  styleUrls: ['./pizza-add.component.scss']
+  styleUrls: ['./pizza-add.component.scss'],
 })
 export class PizzaAddComponent {
+  toppings: string[] = [];
+  constructor(private _bottomSheetRef: MatBottomSheetRef<PizzaAddComponent>) {}
+  createPizza(): void {
+    this._bottomSheetRef.dismiss();
+  }
 
+  addTopping(topping: string): void {
+    (
+      document.getElementById('topping')! as HTMLSelectElement
+    ).selectedIndex = 0;
+    if (this.toppings.includes(topping)) {
+      return;
+    }
+    this.toppings.push(topping);
+  }
+
+  removeTopping(index: number): void {
+    if (index === 0) {
+      this.toppings.shift();
+      return;
+    }
+
+    this.toppings = this.toppings.splice(index, 1);
+  }
 }
--- src/app/modules/main/pages/home/pizza-crud/pizza-crud.component.ts
+++ src/app/modules/main/pages/home/pizza-crud/pizza-crud.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import {
+  MatBottomSheet,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import { PizzaAddComponent } from '../pizza-add/pizza-add.component';
 
 @Component({
   selector: 'app-pizza-crud',
@@ -7,10 +11,18 @@ import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
   styleUrls: ['./pizza-crud.component.scss'],
 })
 export class PizzaCrudComponent {
-  constructor(private _bottomSheetRef: MatBottomSheetRef<PizzaCrudComponent>) {}
+  constructor(
+    private bottomSheet: MatBottomSheet,
+    private _bottomSheetRef: MatBottomSheetRef<PizzaCrudComponent>
+  ) {}
 
   openLink(event: MouseEvent): void {
     this._bottomSheetRef.dismiss();
     event.preventDefault();
+  }
+
+  openUpdatePizza(): void {
+    this._bottomSheetRef.dismiss();
+    this.bottomSheet.open(PizzaAddComponent);
   }
 }
